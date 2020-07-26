189785e51853ae3e6f67232be4181b29aadba2f0
“Carrot7712“ <carrot7712@gmail.com>
2020-07-26 15:24:48 +0800

    add create todo function

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
+const bodyParser= require('body-parser')
 
 const Todo = require('./models/todo')
 
@@ -28,12 +29,46 @@ db.once('open', () => {
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
+//每一筆請求都需要通過 body-parser 進行前置處理
+app.use(bodyParser.urlencoded({ extended: true }))
+
 app.get('/', (req, res) => {
   //拿到全部的 Todo 資料
   Todo.find()
     .lean()
     .then((todos) => res.render('index', { todos }))
     .catch((error) => console.log(error))
+})
+
+//設定新增頁面的路由
+app.get('/todos/new', (req, res) => {
+  //請 view 引擎去拿 new 樣板
+  return res.render('new')
+})
+
+// 來接住表單資料，並且把資料送往資料庫
+app.post('/todos', (req, res) => {
+  //截取傳過來的東西
+  const name = req.body.name
+
+  //一筆資料，目前只存在伺服器端
+  const todo = new Todo({
+    name,
+  })
+
+  //存回資料庫
+  return todo
+    .save()
+    .then(() => res.redirect('/'))
+    .catch((err) => console.log(err))
+})
+
+app.get('/todos/:id', (req, res) => { 
+  const id = req.params.id
+  return Todo.findById(id)
+    .lean()
+    .then(todo => res.render('detail', { todo }))
+    .catch(error=>console.log(error))
 })
 
 app.listen(3000, () => {
